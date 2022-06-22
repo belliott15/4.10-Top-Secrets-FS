83204b989bcfbf4042fe10c2e14c5a2c046915c6
Beau Elliott <belliott1516@gmail.com>
2022-06-22 16:41:53 -0700

    added test to sign in a user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -47,6 +47,15 @@ describe('backend-express-template routes', () => {
     expect(res.body).toEqual(user);
   });
 
+  it('POST /sessions should sign in a user give a 401 if they are not signed in.', async ()=> {
+    const res = await request(app).get('/api/v1/users');
+
+    expect(res.body).toEqual({
+      message: 'Please sign in to continue',
+      status: 401
+    });
+  });
+
   afterAll(() => {
     pool.end();
   });
