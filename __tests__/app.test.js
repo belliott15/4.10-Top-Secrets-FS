c711dcca31af9c2acb18d7bc44e4f31e041abef0
Beau Elliott <belliott1516@gmail.com>
2022-06-22 14:47:17 -0700

    added test to create new user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,13 +3,43 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const testPerson = {
+  first_name: Billy,
+  last_name: Butcherson, 
+  email: 'billyB@unmarkedgrave.com',
+  password: 'mmmMHMHhhhmmmm'
+};
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('POST / should create a new user', () => {
+    const res = await request(app)
+    .post('/api/v1/users')
+    .send(testPerson);
+    const { first_name, last_name, email } = testPerson;
+    expect(res.body).toEqual({
+      id: expect.any(string),
+      first_name, 
+      last_name, 
+      email
+    });
   });
+
+  // it('POST / should create a new user', () => {
+  //   const res = await request(app)
+  //   .post('/api/v1/users')
+  //   .send(testPerson);
+  //   const { first_name, last_name, email } = testPerson;
+  //   expect(res.body).toEqual({
+  //     id: expect.any(string),
+  //     first_name, 
+  //     last_name, 
+  //     email
+  //   });
+  // });
+
   afterAll(() => {
     pool.end();
   });
